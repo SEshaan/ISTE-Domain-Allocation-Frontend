442b1afcec6f60584d53f0be4f0e51299d214485
SEshaan <greatesh2006@gmail.com>
2026-03-14 14:54:25 +0530

    minor fixes

--- src/pages/public/Landing/Domains.tsx
+++ src/pages/public/Landing/Domains.tsx
@@ -3,13 +3,13 @@ import Header from "../../../components/header";
 function DomainSelection() {
 
   const domains = [
-    { id: 'AP', name: 'App Dev', img: '/Domains/AP.png' },
     { id: 'WD', name: 'Web Dev', img: '/Domains/WD.png' },
     { id: 'IT', name: 'IoT', img: '/Domains/IT.png' },
+    { id: 'XR', name: 'GameDev / XR', img: '/Domains/XR.png' },
     { id: 'ML', name: 'Machine Learning', img: '/Domains/ML.png' },
+    { id: 'AP', name: 'App Dev', img: '/Domains/AP.png' },
     { id: 'DS', name: 'Design', img: '/Domains/DS.png' },
     { id: 'MG', name: 'Motion Graphics', img: '/Domains/MG.png' },
-    { id: 'XR', name: 'GameDev / XR', img: '/Domains/XR.png' },
     { id: 'EL', name: 'Electrical', img: '/Domains/EL.png' },
     { id: 'CP', name: 'Competitive Programming', img: '/Domains/CP.png' },
     { id: 'MT', name: 'Management', img: '/Domains/MT.png' },
